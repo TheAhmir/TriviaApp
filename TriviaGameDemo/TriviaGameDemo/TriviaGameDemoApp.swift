bc49f8cfe510b6f9d8f382fab2346a2bdb8a9950
TheAhmir <100968856+TheAhmir@users.noreply.github.com>
2023-06-29 19:31:23 -0400

    setup

---
+++ TriviaGameDemo/TriviaGameDemo/TriviaGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameDemoApp.swift
+//  TriviaGameDemo
+//
+//  Created by Ahmir Postell on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
